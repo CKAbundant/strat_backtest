cb055e9c5ce999184c1d85d89b14e00aaf70ac50
ckaw <i_am_abundant@yahoo.com.sg>
2025-07-28 13:52:15 +0800

    Successfully run 'test_close_pos_invalid_dates' and 'test_close_pos'.

--- tests/test_exit_method.py
+++ tests/test_exit_method.py
@@ -1,5 +1,6 @@
 """Generate test for concrete implementation of 'ExitStruct'."""
 
+from collections import deque
 from datetime import datetime
 from decimal import Decimal
 from pprint import pformat
@@ -7,8 +8,11 @@ from pprint import pformat
 import pandas as pd
 import pytest
 
+from strat_backtest.base.stock_trade import StockTrade
 from strat_backtest.exit_method import FixedExit
+from strat_backtest.utils.utils import display_open_trades
 from tests.test_fixedexit_utils import gen_test_df
+from tests.test_utils import get_latest_record
 
 
 def test_update_exit_levels(sample_gen_trades):
@@ -42,3 +46,85 @@ def test_update_exit_levels(sample_gen_trades):
         datetime(2025, 4, 14, 0, 0): (Decimal("244.56"), Decimal("177.76")),
         datetime(2025, 4, 17, 0, 0): (Decimal("206.32"), Decimal("187.12")),
     }
+
+
+@pytest.mark.parametrize("entry_dt", [datetime(2025, 4, 9), None])
+def test_close_pos_invalid_dates(sample_gen_trades, open_trades, entry_dt):
+    """Test if 'close_pos' raise KeyError for invalid entry dates."""
+
+    display_open_trades(open_trades)
+
+    record = get_latest_record(sample_gen_trades)
+    print(f"record : \n\n{pformat(record, sort_dicts=False)}\n")
+
+    # Intialize instance of FixedExit
+    fixed_exit = FixedExit()
+
+    with pytest.raises(KeyError):
+        computed_trades, computed_list = fixed_exit.close_pos(
+            open_trades, record["date"], record["close"], entry_dt
+        )
+
+
+def test_close_pos(sample_gen_trades, open_trades):
+    """Test if 'close_pos' correctly close the position with correct entry date."""
+
+    display_open_trades(open_trades)
+
+    record = get_latest_record(sample_gen_trades)
+    print(f"record : \n\n{pformat(record, sort_dicts=False)}\n")
+
+    # Intialize instance of FixedExit
+    fixed_exit = FixedExit()
+    entry_dt = datetime(2025, 4, 8)
+
+    computed_trades, computed_list = fixed_exit.close_pos(
+        open_trades, record["date"], record["close"], entry_dt
+    )
+
+    print(f"Computed trades : \n\n{pformat(computed_trades, sort_dicts=False)}\n")
+    print(f"Computed list : \n\n{pformat(computed_list, sort_dicts=False)}\n")
+
+    expected_trades = deque(
+        [
+            StockTrade(
+                ticker="AAPL",
+                entry_datetime=datetime(2025, 4, 7, tzinfo=None),
+                entry_action="buy",
+                entry_lots=10,
+                entry_price=181.46,
+            ),
+            StockTrade(
+                ticker="AAPL",
+                entry_datetime=datetime(2025, 4, 11, tzinfo=None),
+                entry_action="buy",
+                entry_lots=10,
+                entry_price=198.15,
+            ),
+        ]
+    )
+
+    expected_list = [
+        {
+            "ticker": "AAPL",
+            "entry_datetime": datetime(2025, 4, 8, 0, 0),
+            "entry_action": "buy",
+            "entry_lots": Decimal("10"),
+            "entry_price": Decimal("172.42"),
+            "exit_datetime": datetime(2025, 4, 17, 0, 0),
+            "exit_action": "sell",
+            "exit_lots": Decimal("10"),
+            "exit_price": Decimal("196.72"),
+            "days_held": 9,
+            "profit_loss": Decimal("24.30"),
+            "percent_ret": Decimal("0.140935"),
+            "daily_ret": Decimal("0.014758"),
+            "win": 1,
+        }
+    ]
+
+    print(f"Expected trades : \n\n{pformat(expected_trades, sort_dicts=False)}\n")
+    print(f"Expected list : \n\n{pformat(expected_list, sort_dicts=False)}\n")
+
+    assert computed_trades == expected_trades
+    assert computed_list == expected_list
